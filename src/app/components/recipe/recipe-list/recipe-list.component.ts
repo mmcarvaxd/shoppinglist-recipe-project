f6e0a81b9e2ba96b2040bc93060908d589ce7fb0
Matheus Carvalho <matheus.carvalho@mobilemed.com.br>
2021-02-09 18:46:50 -0300

    implemented base layout of recipe component

--- src/app/components/recipe/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Receita de bolo", "Bolo de chocolate muito gostoso", "https://thestayathomechef.com/wp-content/uploads/2016/06/The-Most-Amazing-Chocolate-Cake-Square-1.jpg"),
+  ]
 
   constructor() { }
 
