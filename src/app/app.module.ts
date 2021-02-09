e7bc4b0dee9cc7f195580af4f6c210bbcd47b42b
Matheus Carvalho <matheus.carvalho@mobilemed.com.br>
2021-02-09 17:49:04 -0300

    implmented first project layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { RecipeListComponent } from './components/recipe/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './components/recipe/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './components/recipe/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './components/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    HeaderComponent,
+    RecipeComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: 'shopping-list.component.html',
+    styleUrls: ['shopping-list.component.css'],
+    selector: 'app-shopping-list'
+})
+export class ShoppingListComponent {
+    constructor() {}
+}
